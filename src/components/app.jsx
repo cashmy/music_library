b74dfd5fa52b15a3b87494dda0eefc1ccd98775f
cashmy <cmyers880@gmail.com>
2021-04-22 10:36:23 -0500

    Renamed App. Moved button mapping to music table component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -21,29 +21,7 @@ class App extends Component {
         this.setState({ 
             songs: response.data
         })
-        // this.mapSongsWithButton()
     }
-
-    // handleOnClickEdit = () => {
-    //     editSong.current.toggle();
-    // }
-
-    // mapSongsWithButton() {
-    //     let newSongsMapResult = this.state.songs.map((song) => {
-    //         song.action =   <MDBBtn
-    //                             icon='edit'
-    //                             className='cyan-text'
-    //                             size='lg'
-    //                             style={{ cursor: 'pointer' }}
-    //                             type="button"
-    //                             onClick={this.handleOnClickEdit}
-    //                         />;
-    //         return song;
-    //     });
-    //     this.setState({songs: newSongsMapResult});
-    //     console.log(this.state.songs)
-    //     return
-    // }
     
     render() {
         return (
